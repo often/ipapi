a9d7770c6e2eeebb38650110f04b85589586582f
Sany <44934721+often@users.noreply.github.com>
2020-06-01 05:51:27 -0600

    Added full API coverage

--- mod.js
+++ mod.js
@@ -1,7 +1,12 @@
-export default async function ipapi(ip) {
+export default async function ipapi(ip, lang) {
 	if (!ip) ip = ''
+	if (!lang) lang = 'en'
 
-	const data = await fetch('http://ip-api.com/json/' + ip)
+	const langs = ['en', 'de', 'es', 'pt-BR', 'fr', 'ja', 'zh-CN', 'ru']
+
+	if (!langs.includes(lang)) throw 'unknown language, supported ones are: ' + langs.join(', ')
+
+	const data = await fetch('http://ip-api.com/json/' + ip + '?lang=' + lang + '&fields=66846719')
 
 	return data.json()
 }
