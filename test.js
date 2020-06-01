4e853f849c238c3ea8d5a6ec0bc1e615763ebca3
Sany <44934721+often@users.noreply.github.com>
2020-06-01 05:30:27 -0600

    Update test.js
    add more examples

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
 import ipapi from './mod.js' // import the ipapi module
 
-console.log(await ipapi('1.3.3.7')) // {"status":"success","country":"China","countryCode":"CN","region":"GD","regionName":"Guangdong","city":"Guangzhou Shi","zip":"","lat":23.379,"lon":113.763,"timezone":"Asia/Shanghai","isp":"CHINANET-GD","org":"Chinanet GD","as":"","query":"1.3.3.7"}
+console.log(await ipapi()) // returns information about your ip address
+console.log(await ipapi('1.1.1.1')) // {"status":"success","country":"Australia","countryCode":"AU","region":"NSW","regionName":"New South Wales","city":"Sydney","zip":"1001","lat":-33.8688,"lon":151.209,"timezone":"Australia/Sydney","isp":"Cloudflare, Inc.","org":"","as":"AS13335 Cloudflare, Inc.","query":"1.1.1.1"}
+console.log(await ipapi('2606:4700:4700::1111')) // {"status":"success","country":"United States","countryCode":"US","region":"CA","regionName":"California","city":"San Francisco","zip":"94107","lat":37.7802,"lon":-122.391,"timezone":"America/Los_Angeles","isp":"Cloudflare, Inc.","org":"Cloudflare, Inc.","as":"AS13335 Cloudflare, Inc.","query":"2606:4700:4700::1111"}
+console.log(await ipapi('1.1.1.')) // {"status":"fail","message":"invalid query","query":"1.1.1."}
